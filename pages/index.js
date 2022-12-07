9d70d4bc36e2e2ec36fc7ce2d719652f6884539f
najish-syncoria <najish@mirinfosys.com>
2022-12-07 18:32:17 +0600

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,25 @@ import EarnedWageHero from "../components/Home/EarnedWageHero";
 import RequestDemo from "../components/Home/RequestDemo";
 import HowToUse from "../components/Home/HowToUse";
 import Banner from "../components/Home/Banner";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <>
-      <Banner />
-      <AboutOgreem />
-      <EarnedWageHero />
-      <Benefits />
-      {/* <NumericMetrics /> */}
-      {/* <Partners /> */}
-      <HowToUse />
-      <RequestDemo />
-      {/* <EmployeesBenefit />
+    return (
+        <>
+            <Head>
+                <title>Ogreem</title>
+                <meta property="og:title" content="Ogreem" key="title" />
+            </Head>
+            <Banner />
+            <AboutOgreem />
+            <EarnedWageHero />
+            <Benefits />
+            {/* <NumericMetrics /> */}
+            {/* <Partners /> */}
+            <HowToUse />
+            <RequestDemo />
+            {/* <EmployeesBenefit />
       <EmployersBenefit /> */}
-    </>
-  );
+        </>
+    );
 }
